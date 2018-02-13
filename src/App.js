174abf5ed686bdf30966f324be00ec1f77a6aea1
Jacob Ernst <akumadude.je@gmail.com>
2018-02-13 15:28:31 -0600

    adding more styling for iloveadri

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import heart from './heart.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="adri-jacob">
+            <h1 className="App-title">Adri</h1>
+            <img src={heart} className="App-logo" alt="logo" />
+            <h1 className="App-title">Jacob</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
